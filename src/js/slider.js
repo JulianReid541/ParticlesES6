4baa04923ae525a3698bd1cccf360f83149f15cf
Julian Reid <julianreid@Julians-MacBook-Pro.local>
2019-05-02 17:15:47 -0700

    added code. Still not working

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,121 +1,197 @@
 import './general';
 
-class Particles {
+class particles {
   constructor() {
-    let canvas = document.querySelector('canvas'),
-      ctx = canvas.getContext('2d'),
-      particles = [],
-      particlesNum = 500,
-      w = 500,
-      h = 500,
-      colors = ['#f35d4f', '#f36849', '#c0d988', '#6ddaf1', '#f1e85b'];
+    let canvas = document.querySelector('canvas')
+    let ctx = canvas.getContext('2d')
+    let particles = []
+    let patriclesNum = 500
+    let w = 500
+    let h = 500
+    let colors = ['#f35d4f', '#f36849', '#c0d988', '#6ddaf1', '#f1e85b'];
 
-    this.canvas.width = 500;
-    this.canvas.height = 500;
+    canvas.width = 500;
+    canvas.height = 500;
     canvas.style.left = (window.innerWidth - 500) / 2 + 'px';
 
     if (window.innerHeight > 500)
       canvas.style.top = (window.innerHeight - 500) / 2 + 'px';
 
-    this.init = this.init.bind(this);
-    this.factory = this.factory.bind(this);
-    this.draw = this.Factory.bind(this);
+
+    this.init = this.init.bind(this, patriclesNum, particles);
+    this.factory = this.factory.bind(this, colors, w, h);
+    this.draw = this.factory.bind(this, ctx);
     this.findDistance = this.findDistance.bind(this);
 
-    init();
-    factory(colors, w, h);
-    draw(ctx, particles, particlesNum);
-
+    init;
   }
 
-  init() {
-    for (var i = 0; i < patriclesNum; i++) {
-      particles.push(new Factory);
+  init(){
+    for(let i = 0; i < patriclesNum; i++){
+      particles.push(factory());
     }
   }
 
-  factory(colors, w, h) {
-    this.x = Math.round(Math.random() * w);
-    this.y = Math.round(Math.random() * h);
-    this.rad = Math.round(Math.random() * 1) + 1;
-    this.rgba = colors[Math.round(Math.random() * 3)];
-    this.vx = Math.round(Math.random() * 3) - 1.5;
-    this.vy = Math.round(Math.random() * 3) - 1.5;
+  factory(){
+    this.x =  Math.round( Math.random() * w);
+    this.y =  Math.round( Math.random() * h);
+    this.rad = Math.round( Math.random() * 1) + 1;
+    this.rgba = colors[ Math.round( Math.random() * 3) ];
+    this.vx = Math.round( Math.random() * 3) - 1.5;
+    this.vy = Math.round( Math.random() * 3) - 1.5;
   }
 
-  draw(ctx, particles, particlesNum) {
+  draw(){
     ctx.clearRect(0, 0, w, h);
     ctx.globalCompositeOperation = 'lighter';
-    for (var i = 0; i < patriclesNum; i++) {
-      var temp = particles[i];
-      var factor = 1;
+    for(let i = 0;i < patriclesNum; i++){
+      let temp = particles[i];
+      let factor = 1;
+  
+      for(let j = 0; j<patriclesNum; j++){
+  
+         let temp2 = particles[j];
+         ctx.linewidth = 0.5;
+  
+         if(temp.rgba == temp2.rgba && findDistance(temp, temp2)<50){
+            ctx.strokeStyle = temp.rgba;
+            ctx.beginPath();
+            ctx.moveTo(temp.x, temp.y);
+            ctx.lineTo(temp2.x, temp2.y);
+            ctx.stroke();
+            factor++;
+         }
+      }
+  
+  
+      ctx.fillStyle = temp.rgba;
+      ctx.strokeStyle = temp.rgba;
+  
+      ctx.beginPath();
+      ctx.arc(temp.x, temp.y, temp.rad*factor, 0, Math.PI*2, true);
+      ctx.fill();
+      ctx.closePath();
+  
+      ctx.beginPath();
+      ctx.arc(temp.x, temp.y, (temp.rad+5)*factor, 0, Math.PI*2, true);
+      ctx.stroke();
+      ctx.closePath();
+  
+  
+      temp.x += temp.vx;
+      temp.y += temp.vy;
+  
+      if(temp.x > w)temp.x = 0;
+      if(temp.x < 0)temp.x = w;
+      if(temp.y > h)temp.y = 0;
+      if(temp.y < 0)temp.y = h;
+    }
+  }
 
-      for (var j = 0; j < paxiclesNum; j++) {
+  findDistance(p1,p2){
+    return Math.sqrt( Math.pow(p2.x - p1.x, 2) + Math.pow(p2.y - p1.y, 2) );
+  }
 
-        var temp2 = particles[j];
-        ctx.linewidth = 0.5;
+}
+/*
+var   canvas = document.querySelector('canvas'),
+         ctx = canvas.getContext('2d'),
+   particles = [],
+patriclesNum = 500,
+           w = 500,
+           h = 500,
+      colors = ['#f35d4f','#f36849','#c0d988','#6ddaf1','#f1e85b'];
 
-        if (temp.rgba == temp2.rgba && findDistance(temp, temp2) < 50) {
+canvas.width = 500;
+canvas.height = 500;
+canvas.style.left = (window.innerWidth - 500)/2+'px';
+
+if(window.innerHeight>500)
+canvas.style.top = (window.innerHeight - 500)/2+'px';
+
+function Factory(){
+  this.x =  Math.round( Math.random() * w);
+  this.y =  Math.round( Math.random() * h);
+  this.rad = Math.round( Math.random() * 1) + 1;
+  this.rgba = colors[ Math.round( Math.random() * 3) ];
+  this.vx = Math.round( Math.random() * 3) - 1.5;
+  this.vy = Math.round( Math.random() * 3) - 1.5;
+}
+
+function draw(){
+  ctx.clearRect(0, 0, w, h);
+  ctx.globalCompositeOperation = 'lighter';
+  for(var i = 0;i < patriclesNum; i++){
+    var temp = particles[i];
+    var factor = 1;
+
+    for(var j = 0; j<patriclesNum; j++){
+
+       var temp2 = particles[j];
+       ctx.linewidth = 0.5;
+
+       if(temp.rgba == temp2.rgba && findDistance(temp, temp2)<50){
           ctx.strokeStyle = temp.rgba;
           ctx.beginPath();
           ctx.moveTo(temp.x, temp.y);
           ctx.lineTo(temp2.x, temp2.y);
           ctx.stroke();
           factor++;
-        }
-      }
-
-
-      ctx.fillStyle = temp.rgba;
-      ctx.strokeStyle = temp.rgba;
-
-      ctx.beginPath();
-      ctx.arc(temp.x, temp.y, temp.rad * factor, 0, Math.PI * 2, true);
-      ctx.fill();
-      ctx.closePath();
-
-      ctx.beginPath();
-      ctx.arc(temp.x, temp.y, (temp.rad + 5) * factor, 0, Math.PI * 2, true);
-      ctx.stroke();
-      ctx.closePath();
-
-
-      temp.x += temp.vx;
-      temp.y += temp.vy;
-
-      if (temp.x > w) temp.x = 0;
-      if (temp.x < 0) temp.x = w;
-      if (temp.y > h) temp.y = 0;
-      if (temp.y < 0) temp.y = h;
+       }
     }
+
+
+    ctx.fillStyle = temp.rgba;
+    ctx.strokeStyle = temp.rgba;
+
+    ctx.beginPath();
+    ctx.arc(temp.x, temp.y, temp.rad*factor, 0, Math.PI*2, true);
+    ctx.fill();
+    ctx.closePath();
+
+    ctx.beginPath();
+    ctx.arc(temp.x, temp.y, (temp.rad+5)*factor, 0, Math.PI*2, true);
+    ctx.stroke();
+    ctx.closePath();
+
+
+    temp.x += temp.vx;
+    temp.y += temp.vy;
+
+    if(temp.x > w)temp.x = 0;
+    if(temp.x < 0)temp.x = w;
+    if(temp.y > h)temp.y = 0;
+    if(temp.y < 0)temp.y = h;
   }
-
-  loop() {
-    draw();
-    requestAnimFrame(loop);
-  }
-
-  findDistance(p1, p2) {
-    return Math.sqrt(Math.pow(p2.x - p1.x, 2) + Math.pow(p2.y - p1.y, 2));
-  }
-
-/*
-  window.requestAnimFrame = (function () {
-    return window.requestAnimationFrame ||
-      window.webkitRequestAnimationFrame ||
-      window.mozRequestAnimationFrame ||
-      function (callback) {
-        window.setTimeout(callback, 1000 / 60);
-      };
-  })();
-
-  */
-
 }
 
-let particles;
+function findDistance(p1,p2){
+  return Math.sqrt( Math.pow(p2.x - p1.x, 2) + Math.pow(p2.y - p1.y, 2) );
+}
 
-window.onload = () => new Particles;
+window.requestAnimFrame = (function(){
+  return  window.requestAnimationFrame       ||
+          window.webkitRequestAnimationFrame ||
+          window.mozRequestAnimationFrame    ||
+          function( callback ){
+            window.setTimeout(callback, 1000 / 60);
+          };
+})();
+
+(function init(){
+  for(var i = 0; i < patriclesNum; i++){
+    particles.push(new Factory);
+  }
+})();
+
+(function loop(){
+  draw();
+  requestAnimFrame(loop);
+})();
+
+*/
+
+window.onload = () => new particles;
 
 
 
