ae920efd9060e71841475063704c858f3105f00e
JulianReid541git status <reidjulian@outlook.com>
2019-07-03 13:16:56 -0700

    particles are on page

--- src/js/slider.js
+++ src/js/slider.js
@@ -18,8 +18,11 @@ class particles {
 
     this.init = this.init.bind(this);
     this.factory = this.factory.bind(this);
+    this.draw = this.draw.bind(this);
+    this.findDistance = this.findDistance.bind(this);
 
     this.init();
+    this.draw();
   }
 
   init(){
@@ -27,7 +30,7 @@ class particles {
       this.particles.push(this.factory());
     }    
     console.log(this.particles[1]);
-    console.log(this.particles[100]);
+    console.log(this.particles[399]);
   }
 
   factory(){
@@ -39,6 +42,57 @@ class particles {
     let vy = Math.round( Math.random() * 3) - 1.5;
     return {x, y, rad, rgba, vx, vy};
   }
+
+  draw(){
+  this.ctx.clearRect(0, 0, this.w, this.h);
+  this.ctx.globalCompositeOperation = 'lighter';
+  for(var i = 0;i < this.particlesNum; i++){
+    var temp = this.particles[i];
+    var factor = 1;
+
+    for(var j = 0; j<this.particlesNum; j++){
+
+       var temp2 = this.particles[j];
+       this.ctx.linewidth = 0.5;
+
+       if(temp.rgba == temp2.rgba && this.findDistance(temp, temp2)<50){
+          this.ctx.strokeStyle = temp.rgba;
+          this.ctx.beginPath();
+          this.ctx.moveTo(temp.x, temp.y);
+          this.ctx.lineTo(temp2.x, temp2.y);
+          this.ctx.stroke();
+          factor++;
+       }
+    }
+
+
+    this.ctx.fillStyle = temp.rgba;
+    this.ctx.strokeStyle = temp.rgba;
+
+    this.ctx.beginPath();
+    this.ctx.arc(temp.x, temp.y, temp.rad*factor, 0, Math.PI*2, true);
+    this.ctx.fill();
+    this.ctx.closePath();
+
+    this.ctx.beginPath();
+    this.ctx.arc(temp.x, temp.y, (temp.rad+5)*factor, 0, Math.PI*2, true);
+    this.ctx.stroke();
+    this.ctx.closePath();
+
+
+    temp.x += temp.vx;
+    temp.y += temp.vy;
+
+    if(temp.x > this.w)temp.x = 0;
+    if(temp.x < 0)temp.x = this.w;
+    if(temp.y > this.h)temp.y = 0;
+    if(temp.y < 0)temp.y = this.h;
+    }
+  }
+
+  findDistance(p1,p2){
+      return Math.sqrt( Math.pow(p2.x - p1.x, 2) + Math.pow(p2.y - p1.y, 2) );
+    }
 }
 
 
@@ -136,15 +190,6 @@ class particles {
 //   draw();
 //   requestAnimFrame(loop);
 // })();
-
-// window.requestAnimFrame = (function(){
-//     return  window.requestAnimationFrame       ||
-//             window.webkitRequestAnimationFrame ||
-//             window.mozRequestAnimationFrame    ||
-//             function( callback ){
-//               window.setTimeout(callback, 1000 / 60);
-//             };
-//   })();
 
 
 new particles();
