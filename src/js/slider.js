0d3aafe15fba1248346fb8afeb2266b522734b55
JulianReid541git status <reidjulian@outlook.com>
2019-07-03 13:40:22 -0700

    added comments. Project is finished

--- src/js/slider.js
+++ src/js/slider.js
@@ -2,29 +2,42 @@ import './general';
 
 class particles {
   constructor(){
+    //variable declarations
     this.canvas = document.querySelector('canvas'),
     this.ctx = this.canvas.getContext('2d'),
     this.particles = [],
-    this.particlesNum = 500
-    this.w = 500,
-    this.h = 500,
-    this.colors = ['#f35d4f','#f36849','#c0d988','#6ddaf1','#f1e85b'];
+    this.particlesNum = 400,
+    //this.w = 1000,
+    this.w = window.innerWidth,
+    //this.h = 1000,
+    this.h = window.innerHeight,
+    this.colors = ['#f35d4f','#f36849',
+                   '#c0d988','#6ddaf1',
+                   '#f1e85b','#00FF00',
+                   '#ADD8E6','#00FFFF'];
 
-    this.canvas.width = 500;
-    this.canvas.height = 500;
-    this.canvas.style.left = (window.innerWidth - 500)/2+'px';
+    // this.canvas.width = 1000;
+    this.canvas.width = window.innerWidth;
+    // this.canvas.height = 1000;
+    this.canvas.height = window.innerHeight;
+    // this.canvas.style.left = (window.innerWidth - 500)/2+'px';
     if(window.innerHeight > 500)
       this.canvas.style.top = (window.innerHeight - 500)/2+'px';
 
+    //function binds
     this.init = this.init.bind(this);
     this.factory = this.factory.bind(this);
     this.draw = this.draw.bind(this);
     this.findDistance = this.findDistance.bind(this);
+    this.loop = this.loop.bind(this);
 
+    //method calls
     this.init();
     this.draw();
+    this.loop();
   }
 
+  //sets up the array particles by pushing each index to new factory
   init(){
     for(let i = 0; i < this.particlesNum; i++) {
       this.particles.push(this.factory());
@@ -33,6 +46,7 @@ class particles {
     console.log(this.particles[399]);
   }
 
+  //assigns X,Y,RAD,RGBA,VX,VY and returns it in an array
   factory(){
     let x =  Math.round( Math.random() * this.w);
     let y =  Math.round( Math.random() * this.h);
@@ -43,6 +57,7 @@ class particles {
     return {x, y, rad, rgba, vx, vy};
   }
 
+  //draws the particles to the canvas using the info returned from factory 
   draw(){
   this.ctx.clearRect(0, 0, this.w, this.h);
   this.ctx.globalCompositeOperation = 'lighter';
@@ -90,107 +105,25 @@ class particles {
     }
   }
 
+  //finds the distance to the nearest particle
   findDistance(p1,p2){
       return Math.sqrt( Math.pow(p2.x - p1.x, 2) + Math.pow(p2.y - p1.y, 2) );
-    }
+  }
+
+  //requests and animation frame, redraws the canvas. Constant loop. This is what moves the particles
+  loop(){
+      window.requestAnimFrame = (function(){
+        return  window.requestAnimationFrame       ||
+                window.webkitRequestAnimationFrame ||
+                window.mozRequestAnimationFrame    ||
+                function( callback ){
+                  window.setTimeout(callback, 1000 / 60);
+                };
+      })();
+      this.draw();
+      window.requestAnimFrame(this.loop);
+  }
 }
-
-
-// var   canvas = document.querySelector('canvas'),
-//          ctx = canvas.getContext('2d'),
-//    particles = [],
-// particlesNum = 500,
-//            w = 500,
-//            h = 500,
-//       colors = ['#f35d4f','#f36849','#c0d988','#6ddaf1','#f1e85b'];
-
-// canvas.width = 500;
-// canvas.height = 500;
-// canvas.style.left = (window.innerWidth - 500)/2+'px';
-
-// if(window.innerHeight>500)
-// canvas.style.top = (window.innerHeight - 500)/2+'px';
-
-// function Factory(){
-//   this.x =  Math.round( Math.random() * w);
-//   this.y =  Math.round( Math.random() * h);
-//   this.rad = Math.round( Math.random() * 1) + 1;
-//   this.rgba = colors[ Math.round( Math.random() * 3) ];
-//   this.vx = Math.round( Math.random() * 3) - 1.5;
-//   this.vy = Math.round( Math.random() * 3) - 1.5;
-// }
-
-// function draw(){
-//   ctx.clearRect(0, 0, w, h);
-//   ctx.globalCompositeOperation = 'lighter';
-//   for(var i = 0;i < particlesNum; i++){
-//     var temp = particles[i];
-//     var factor = 1;
-
-//     for(var j = 0; j<particlesNum; j++){
-
-//        var temp2 = particles[j];
-//        ctx.linewidth = 0.5;
-
-//        if(temp.rgba == temp2.rgba && findDistance(temp, temp2)<50){
-//           ctx.strokeStyle = temp.rgba;
-//           ctx.beginPath();
-//           ctx.moveTo(temp.x, temp.y);
-//           ctx.lineTo(temp2.x, temp2.y);
-//           ctx.stroke();
-//           factor++;
-//        }
-//     }
-
-
-//     ctx.fillStyle = temp.rgba;
-//     ctx.strokeStyle = temp.rgba;
-
-//     ctx.beginPath();
-//     ctx.arc(temp.x, temp.y, temp.rad*factor, 0, Math.PI*2, true);
-//     ctx.fill();
-//     ctx.closePath();
-
-//     ctx.beginPath();
-//     ctx.arc(temp.x, temp.y, (temp.rad+5)*factor, 0, Math.PI*2, true);
-//     ctx.stroke();
-//     ctx.closePath();
-
-
-//     temp.x += temp.vx;
-//     temp.y += temp.vy;
-
-//     if(temp.x > w)temp.x = 0;
-//     if(temp.x < 0)temp.x = w;
-//     if(temp.y > h)temp.y = 0;
-//     if(temp.y < 0)temp.y = h;
-//   }
-// }
-
-// function findDistance(p1,p2){
-//   return Math.sqrt( Math.pow(p2.x - p1.x, 2) + Math.pow(p2.y - p1.y, 2) );
-// }
-
-// window.requestAnimFrame = (function(){
-//   return  window.requestAnimationFrame       ||
-//           window.webkitRequestAnimationFrame ||
-//           window.mozRequestAnimationFrame    ||
-//           function( callback ){
-//             window.setTimeout(callback, 1000 / 60);
-//           };
-// })();
-
-// (function init(){
-//   for(var i = 0; i < particlesNum; i++){
-//     particles.push(new Factory);
-//   }
-// })();
-
-// (function loop(){
-//   draw();
-//   requestAnimFrame(loop);
-// })();
-
 
 new particles();
 
